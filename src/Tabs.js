201eddf16df09a44b3c60cc33e0caaedb906fef5
William Shen <william.shen.0001@gmail.com>
2024-04-16 18:52:22 -0700

    Merge branch 'main' of https://github.com/chrisjacob-s/test-charts

---
+++ src/Tabs.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./layout.css";
+
+function Tabs(){
+
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) =>{
+        setToggleState(index);
+    }
+
+    
+
+    return(
+        <div className = "container">
+
+            <div className ="bloc-tabs">
+            <div className = {toggleState === 1 ? "tabs active-tabs" : "tabs" }
+            onClick = { () =>toggleTab(1)}
+            >Recommendation</div>
+            <div className = {toggleState === 2 ? "tabs active-tabs" : "tabs" }
+            onClick = { () =>toggleTab(2)}
+            >ROI</div>
+            <div className = {toggleState === 3 ? "tabs active-tabs" : "tabs" }
+            onClick = { () =>toggleTab(3)}
+            >Graph</div>
+        </div>
+            <div className="content-tabs">
+                <div className = {toggleState === 1 ? "content active-content" : "content"}>
+                    <h2>Add Data 1</h2>
+                </div>
+                <div className = {toggleState === 2 ?"content active-content" : "content"}>
+                    <h2>Add Data 2</h2>
+                </div>
+                <div className = {toggleState === 3 ?"content active-content" : "content"}>
+                    <h2>Add Data 3</h2>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Tabs;
