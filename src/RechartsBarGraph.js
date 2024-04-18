ca89c323e54d8a37f288e967a8a2f0dbb01c4410
Christopher Soriano <chrissoriano1313@gmail.com>
2024-04-18 15:33:43 -0700

    added bar graph component

---
+++ src/RechartsBarGraph.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const RechartsBarGraph = () => {
+    const data = [
+        {
+            name: 'Page A',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'Page B',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'Page C',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: 'Page D',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        }
+    ];
+
+    return (
+        <></>
+    )
+}
+
+export default RechartsBarGraph
