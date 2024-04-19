ee108640550c7f3f12d2cc9e7ed0057c8940c027
Christopher Soriano <chrissoriano1313@gmail.com>
2024-04-18 22:19:27 -0700

    weird commit error

--- src/Tabs.js
+++ src/Tabs.js
@@ -11,48 +11,14 @@ function Tabs() {
     console.log(index);
   };
 
-<<<<<<< HEAD
   return (
-    <div className="container h-5/6">
+    <div className="container h-2/3 ">
       <div className="bloc-tabs mb-2">
         <div
           className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(1)}
         >
           Recommendation
-=======
-    const toggleTab = (index) => {
-        setToggleState(index);
-        console.log(index);
-    }
-
-
-    return (
-        <div className="container h-2/3 ">
-
-            <div className="bloc-tabs mb-2">
-                <div className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-                    onClick={() => toggleTab(1)}
-                >Recommendation</div>
-                <div className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-                    onClick={() => toggleTab(2)}
-                >ROI</div>
-                <div className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-                    onClick={() => toggleTab(3)}
-                >Graph</div>
-            </div>
-            <div className="flex h-full">
-                <div className={toggleState === 1 ? "active-content" : "content"}>
-                    <TitleTabs />
-                </div>
-                <div className={toggleState === 2 ? "active-content" : "content"}>
-                    <ROICOMPS />
-                </div>
-                <div className={toggleState === 3 ? "active-content" : "content"}>
-                    <RechartsBarGraph />
-                </div>
-            </div>
->>>>>>> 88776fb95fee8512c2069e15b964409e7f79bcff
         </div>
         <div
           className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
