f7273da013bd8bfa4be1258ecd8d0b05ddebaf5e
William Shen <william.shen.0001@gmail.com>
2024-04-23 08:52:22 -0700

    add table from mui

---
+++ src/InnerTabComponents/PayoutTable.js
@@ -0,0 +1,12 @@
+import { useState, useEffect, useMemo } from "react";
+import {MaterialReactTable, useMaterialReactTable} from "material-react-table"
+
+export const PayoutTable = () =>{
+    const coulumns = useMemo(
+        ()=>{
+            
+        }
+
+    )
+
+}
